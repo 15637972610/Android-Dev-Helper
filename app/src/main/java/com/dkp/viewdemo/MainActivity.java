35c59b183e1da16f3d3c59f40331e231abb8fb7e
dkp17801090519 <dukunpeng@chinasofti.com>
2019-05-10 06:12:39 +0800

    nothing

--- app/src/main/java/com/dkp/viewdemo/MainActivity.java
+++ app/src/main/java/com/dkp/viewdemo/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //https://blog.csdn.net/stevennest/article/details/77877074
     }
 }
